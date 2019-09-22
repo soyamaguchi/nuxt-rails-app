e87faa0c139af33c0c437cab5bce010cfcaecfc6
Soya Yamaguchi <f.soya.maguchi@gmail.com>
2019-09-22 23:48:00 +0900

    Delete 不要なファイルや記述

--- frontend/plugins/axios.js
+++
@@ -1,7 +0,0 @@
-// export default function({ $axios, redirect }) {
-//   $axios.setToken('access_token')
-
-//   $axios.onResponse((config) => {
-//     $axios.setHeader('Access-control-Allow-Origin', 'http://localhost:8080')
-//   })
-// }
